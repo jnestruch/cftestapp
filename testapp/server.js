4d78d9e5538a0506fd4c66e00924c5c6fb726571
j.estruch <jnestruch@gmail.com>
2020-11-20 09:33:41 +0100

    adding Travis

--- testapp/server.js
+++ testapp/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Test app for cloud foundry!');
+  res.send('Test app for cloud foundry v1.0!');
 });
 
 const port = process.env.PORT || 3000;
